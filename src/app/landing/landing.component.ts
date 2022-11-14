e9150ad6fce8a352415c0bace360eeac21a3b2c6
Juan Carlos Barrera B <51795550+Juancarlos56@users.noreply.github.com>
2022-11-14 00:44:00 -0500

    landing page con novedades

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Proyectos } from '../models/proyectos';
 import data from '../../assets/data/proyectos/paginas.json';
+import { Skills } from '../models/skill';
+import lenguaje from '../../assets/data/skills/lenguajesProgramacion.json'
+import databases from '../../assets/data/skills/databases.json'
+import controversion from '../../assets/data/skills/controlVersiones.json'
+import metologias from '../../assets/data/skills/metodologias.json'
+import frameworks from '../../assets/data/skills/framework.json'
+import integracion from '../../assets/data/skills/controlIntegracion.json'
+import servidoreslist from '../../assets/data/skills/servidores.json'
+
 
 @Component({
     selector: 'app-landing',
@@ -12,12 +21,27 @@ export class LandingComponent implements OnInit {
   focus: any;
   focus1: any;
   listaProyectos: Array<Proyectos> = []
-  
+  listaLenguajeProgramacion: Array<Skills> = []
+  listaBasesDatos: Array<Skills> = []
+  listaControlVersion: Array<Skills> = []
+  listaMetodologias: Array<Skills> = []
+  listaFrameworks: Array<Skills> = []
+  listaIntegracion: Array<Skills> = []
+  listaServidores: Array<Skills> = []
+
+
   constructor() { }
 
   ngOnInit() {
 
     this.listaProyectos  = data as Array<Proyectos>;
+    this.listaLenguajeProgramacion = lenguaje as Array<Skills>;
+    this.listaBasesDatos = databases as Array<Skills>;
+    this.listaControlVersion = controversion as Array<Skills>;
+    this.listaMetodologias = metologias as Array<Skills>;
+    this.listaFrameworks = frameworks as Array<Skills>;
+    this.listaIntegracion = integracion as Array<Skills>;
+    this.listaServidores = servidoreslist as Array<Skills>;
     
   }
  
---
+++ src/app/models/skill.ts
@@ -0,0 +1,7 @@
+export interface Skills{
+    //Atributos 
+    id: number;
+    nombres: String;
+    porcentaje: number;
+    icono: String;
+}
